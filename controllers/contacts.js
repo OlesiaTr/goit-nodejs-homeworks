275882f6c603749e16c7e1285bca5e9dc9e96f33
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2023-03-07 18:05:44 +0200

    fix: fix updateContact controller

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -31,7 +31,7 @@ const updateContact = async (req, res, next) => {
   const { contactId } = req.params;
   const result = await contacts.updateContact(contactId, req.body);
   if (!result) throw HttpError(404, 'Not found');
-  res.json(res);
+  res.json(result);
 };
 
 module.exports = {
