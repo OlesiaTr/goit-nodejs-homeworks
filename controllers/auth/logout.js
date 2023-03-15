2ca7566252bfe1b3eb8997de9b7a64b0c7d0c638
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2023-03-15 17:54:31 +0200

    feat: add logout route

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -1,3 +1,10 @@
-const logout = async (req, res, next) => {};
+const { User } = require('../../models');
+
+const logout = async (req, res) => {
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: '' });
+
+  res.status(204);
+};
 
 module.exports = logout;
