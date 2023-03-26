40fdf7717e051e21b3610463714d22b0b03c469b
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2023-03-26 19:21:37 +0300

    Revert "feat: add an email to the user with a verification link"

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -7,15 +7,11 @@ const router = express.Router();
 
 router.get('/current', authenticator, authCtrls.getCurrent);
 
-router.get('/verify/:verificationToken', authCtrls.verifyEmail);
-
 router.post('/signup', validateBody(userJoi.singUpSchema), authCtrls.signup);
 
 router.post('/signin', validateBody(userJoi.signInSchema), authCtrls.signin);
 
 router.post('/logout', authenticator, authCtrls.logout);
-
-router.post('/verify', authCtrls.resendEmail);
 
 router.patch(
   '/',
